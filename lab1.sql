050058acce1da2c06c75fe18d0c51b820b216e0d
SamuelStrand <emil.vakhitov@list.ru>
2024-09-10 16:49:44 +0500

    updated but not finished

--- lab1.sql
+++ lab1.sql
@@ -1,5 +1,25 @@
 CREATE DATABASE lab1;
 
-CREATE TABLE users;
+CREATE TABLE users(
+    id SERIAL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL
+);
 
-INSERT INTO users values 
+ALTER TABLE users
+ADD COLUMN is_admin INTEGER;
+
+
+-- INSERT INTO users (firstname, lastname, is_admin)
+-- VALUES
+--     ('Alice', 'A', 0),
+--     ('Bob',  'B', 1),
+--     ('Charlie', 'C', 0);
+
+
+ALTER TABLE users
+ALTER COLUMN is_admin TYPE BOOLEAN
+USING is_admin::boolean;
+
+SELECT * FROM users;
+
