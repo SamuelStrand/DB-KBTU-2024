a7f62b60467f1333e04b976d2c19c2f35c178355
SamuelStrand <emil.vakhitov@list.ru>
2024-09-17 00:04:35 +0500

    finished lab1

--- lab1.sql
+++ lab1.sql
@@ -6,20 +6,23 @@ CREATE TABLE users(
     lastname VARCHAR(50) NOT NULL
 );
 
-ALTER TABLE users
-ADD COLUMN is_admin INTEGER;
-
-
--- INSERT INTO users (firstname, lastname, is_admin)
--- VALUES
---     ('Alice', 'A', 0),
---     ('Bob',  'B', 1),
---     ('Charlie', 'C', 0);
-
+ALTER TABLE users ADD COLUMN is_admin INTEGER;
 
 ALTER TABLE users
 ALTER COLUMN is_admin TYPE BOOLEAN
 USING is_admin::boolean;
 
+ALTER TABLE users
+ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY ,
+    name VARCHAR(50),
+    user_id INT
+);
+
 SELECT * FROM users;
 
+DROP TABLE tasks;
+
+DROP DATABASE lab1;
